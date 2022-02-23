3f39d4d1119f25d53be2a58187496a5b4221f997
jagwarthegreat <jguapin21@gmail.com>
2022-02-23 23:19:14 +0800

    added btn

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <button class="inline-flex items-center px-4 py-2 mt-4 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3"> Create </button>
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="table-auto w-full border-collapse border border-slate-400">
                         <thead>
